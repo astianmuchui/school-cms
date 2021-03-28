2e3629fd0bc0cbfe9472efea27947946e3b68540
Seb Astian <Sebastianmuchui79@gmail.com>
2021-03-28 15:27:10 +0300

    new sql structure

--- login_system.sql
+++ login_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2021 at 10:35 PM
+-- Generation Time: Mar 28, 2021 at 02:25 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -39,7 +39,42 @@ CREATE TABLE `assignments` (
 --
 
 INSERT INTO `assignments` (`id`, `title`, `doc_file`, `time`) VALUES
-(1, 'Biology', 'BIOLOGY-REVISION-BOOKLET.pdf', '2021-03-25 21:08:16.106387');
+(11, 'business assignment 2', 'BUSINESS-STUDIES-NOTES-F3.docx', '2021-03-27 10:09:38.975477'),
+(19, 'Essays Assignment', 'BIOLOGY-ESSAY-QUESTIONS-EXPECTED-ANSWERS.pdf', '2021-03-27 20:53:42.046014');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exams`
+--
+
+CREATE TABLE `exams` (
+  `id` int(255) NOT NULL,
+  `exam_name` varchar(255) NOT NULL,
+  `exam_file` varchar(255) NOT NULL,
+  `time` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `exams`
+--
+
+INSERT INTO `exams` (`id`, `exam_name`, `exam_file`, `time`) VALUES
+(1, 'End term 3 2020', 'FORM-2-BIOLOGY.docx', '2021-03-26 09:47:53.358186'),
+(2, 'Chemistry paper 3', 'KCSE-FORM-3-CHEMISTRY-NOTES.pdf', '2021-03-26 14:28:45.064035');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mail_records`
+--
+
+CREATE TABLE `mail_records` (
+  `id` int(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `mail_message` varchar(255) NOT NULL,
+  `send_time` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -53,7 +88,7 @@ CREATE TABLE `users` (
   `adm_no` int(255) NOT NULL,
   `passcode` varchar(255) NOT NULL,
   `user_mail` varchar(255) NOT NULL,
-  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -61,8 +96,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `adm_no`, `passcode`, `user_mail`, `created_at`) VALUES
-(2, 'Leona Lewis', 4565, '19844', 'leonalewis6@gmail.com', '2021-03-25 13:42:40.561240'),
-(3, 'becky jayne', 6879, '0021', 'becky891@gmail.com', '2021-03-25 21:31:55.128092');
+(2, 'michaela stone', 10958, '1205', 'test@test.com', '2021-03-28 12:24:18.305904'),
+(14, 'Cal stone', 8280, '6523', 'calstone18@gmail.com', '2021-03-28 11:14:28.860571'),
+(15, 'Jared vasques', 5112, '8280', 'jaredvasques5@gmail.com', '2021-03-28 11:57:07.127853');
 
 --
 -- Indexes for dumped tables
@@ -72,6 +108,18 @@ INSERT INTO `users` (`user_id`, `username`, `adm_no`, `passcode`, `user_mail`, `
 -- Indexes for table `assignments`
 --
 ALTER TABLE `assignments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `exams`
+--
+ALTER TABLE `exams`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mail_records`
+--
+ALTER TABLE `mail_records`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -88,13 +136,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `exams`
+--
+ALTER TABLE `exams`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `mail_records`
+--
+ALTER TABLE `mail_records`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
